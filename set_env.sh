fe3492856a2e1061c1f9d7c5dcdf552f213dcff6
KlanMcKenzie <80576186+KlanMcKenzie@users.noreply.github.com>
2022-10-01 18:16:04 +0300

    Updated set_env var

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=myusername
 export POSTGRES_PASSWORD=mypasssword
-export POSTGRES_HOST=database-1.comb4hoh2nqs.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.cg8nfqooxpk0.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mono123456
+export AWS_BUCKET=mymonolithbucket3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
